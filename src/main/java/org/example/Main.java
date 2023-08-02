a53e40add6af6dae3fb2209eec47882ec1c7efcd
AntonioSedicina <user24xyz@gmail.com>
2023-08-02 16:53:51 +0200

    repo2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to repo2");
+        System.out.println("porco dio non funziona");
     }
 }
