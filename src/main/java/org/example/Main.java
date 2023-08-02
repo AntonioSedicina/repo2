e358a787aca6df3789216ea134be8c79a837a7e5
AntonioSedicina <user24xyz@gmail.com>
2023-08-02 16:53:51 +0200

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcome to repo2");
+        System.out.println("Welcome to repo2");
     }
 }
